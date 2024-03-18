f0ed36c0dcfc6aaa952d10f514a05bcf0910abd2
samrocker <samrockerislive04@gmail.com>
2024-03-19 02:18:27 +0530

    third Push

---
+++ script.js
@@ -0,0 +1,4 @@
+let NavToggle = document.getElementById('SlideBar-toggle-btn')
+NavToggle.addEventListener('click' , function(){
+    document.getElementById('SlideBar').classList.toggle('active')
+})
